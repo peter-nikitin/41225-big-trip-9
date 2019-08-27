e1ac25071118608d9fd4e64a46f26b7486c676a0
Никитин Петр <nikitin@MacBook-Pro-Admin.local>
2019-08-27 21:10:59 +0300

    add dynamics day renderring

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { tripInfo as tripInfoLayout } from './components/tripInfo.js';
 import { menu as menuLayout } from './components/menu.js';
 import { filters as filtersLayout } from './components/filters.js';
-import { eventEdit as eventEditLayout } from './components/eventEdit.js';
-import { eventItem as eventItemLayout } from './components/eventItem.js';
-import { sort as sortLayout } from './components/sort.js';
-import { getPoint } from './data.js';
 
-const render = (container, element, position) => {
-  container.insertAdjacentHTML(position, element);
-};
+
+import { sort as sortLayout } from './components/sort.js';
+import { getPoint, getFilter, getDays } from './data.js';
+import { renderTripDay } from './renderTripDay.js'
+import { render } from './render.js'
+
+
 
 const tripInfo = document.querySelector(`.trip-main__trip-info `);
 const menu = document.querySelector(`.trip-main__trip-controls`);
@@ -16,56 +16,17 @@ const tripEvents = document.querySelector(`.trip-events`);
 
 render(tripInfo, tripInfoLayout(), `afterbegin`);
 render(menu, menuLayout(), `afterbegin`);
-render(menu, filtersLayout(), `beforeend`);
+render(menu, filtersLayout(getFilter()), `beforeend`);
 render(tripEvents, sortLayout(), `afterbegin`);
+
+const days = getDays(3, new Array(3).fill(``).map(() => getPoint(Math.floor(Math.random() * 100))))
+const tripPonts = new Array(3).fill(``).map(() => getPoint(Math.floor(Math.random() * 100)));
+
+console.log(days);
+
 
 const tripDays = document.createElement(`ul`);
 tripDays.classList.add(`trip-days`);
 
-const renderTripDay = (dayNumber, eventsCount) => {
-  const tripDyaItem = document.createElement(`li`);
-  tripDyaItem.classList.add(`trip-days__item`);
-  tripDyaItem.classList.add(`day`);
-
-  const dayInfo = document.createElement(`div`);
-  dayInfo.classList.add(`day__info`);
-
-  const counter = document.createElement(`span`);
-  counter.classList.add(`day__counter`);
-  counter.innerText = dayNumber;
-
-  const dateElement = document.createElement(`time`);
-  dateElement.classList.add(`day__date`);
-  dateElement.dateTime = `2019-03-18`;
-  dateElement.innerText = `MAR 18`;
-
-  dayInfo.appendChild(counter);
-  dayInfo.appendChild(dateElement);
-
-  const tripEventsList = document.createElement(`ul`);
-  tripEventsList.classList.add(`trip-events__list`);
-
-  const tripEventsItemEdit = document.createElement(`li`);
-  tripEventsItemEdit.classList.add(`trip-events__item`);
-
-  render(tripEventsItemEdit, eventEditLayout(), `afterbegin`);
-  tripEventsList.appendChild(tripEventsItemEdit);
-
-  const tripPonts = new Array(3).fill(``).map(() => getPoint(Math.floor(Math.random() * 100)));
-
-  tripPonts.map((point) => {
-    const tripEventsItem = document.createElement(`li`);
-    tripEventsItem.classList.add(`trip-events__item`);
-    render(tripEventsItem, eventItemLayout(point, [Math.floor(Math.random() * 2), Math.floor(Math.random() * 2)]), `afterbegin`);
-    tripEventsList.appendChild(tripEventsItem);
-  })
-
-
-  tripDyaItem.appendChild(dayInfo);
-  tripDyaItem.appendChild(tripEventsList);
-
-  return tripDyaItem;
-};
-
-tripDays.appendChild(renderTripDay(1, 3));
+days.map((day, index) => tripDays.appendChild(renderTripDay(index + 1, day.points)));
 tripEvents.appendChild(tripDays);
