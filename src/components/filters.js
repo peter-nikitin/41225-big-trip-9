df462b2653d19c9b19514ac0af0e6632cd04df4c
Никитин Петр <nikitin@MacBook-Pro-Admin.local>
2019-08-27 21:10:42 +0300

    refacto filter

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,20 +1,12 @@
-export const filters = () => `
+export const filters = (filters) => `
 <form class="trip-filters" action="#" method="get">
-  <div class="trip-filters__filter">
-    <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-    <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-  </div>
+${filters.map((filter) => `
+<div class="trip-filters__filter">
+  <input id="filter-${filter.toLowerCase()}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter.toLowerCase()}">
+  <label class="trip-filters__filter-label" for="filter-${filter.toLowerCase()}">${filter}</label>
+</div>
 
-  <div class="trip-filters__filter">
-    <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-    <label class="trip-filters__filter-label" for="filter-future">Future</label>
-  </div>
-
-  <div class="trip-filters__filter">
-    <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-    <label class="trip-filters__filter-label" for="filter-past">Past</label>
-  </div>
-
+`).join(``)}
   <button class="visually-hidden" type="submit">Accept filter</button>
 </form>
 `;
