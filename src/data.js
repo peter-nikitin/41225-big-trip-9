ccc889d35ecafb2695c1ebcab47af0070b9f3c0c
Никитин Петр <nikitin@MacBook-Pro-Admin.local>
2019-08-29 22:45:51 +0300

    add more data lists

--- src/data.js
+++ src/data.js
@@ -1,48 +1,46 @@
-export const getPoint = (price) => ({
-  icon: [
-    `bus`,
-    `check-in`,
-    `check-in`,
-    `drive`,
-    `flight`,
-    `restaurant`,
-    `ship`,
-    `sightseeing`,
-    `taxi`,
-    `train`,
-    `transport`,
-    `trip`
-  ][Math.floor(Math.random() * 12)],
-  action: [`Taxi to`, `Flight to`, `Drive to`, `Check into`, `Natural History Museum`, ``][
-    Math.floor(Math.random() * 6)
-  ],
-  city: [`Moscow`, `Sain-Petersburg`, `Porto`, `Lisboa`][
-    Math.floor(Math.random() * 4)
-  ],
-  image: `http://picsum.photos/300/150?r=${Math.random()}`,
-  description: [
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-    `Cras aliquet varius magna, non porta ligula feugiat eget.`,
-    `Fusce tristique felis at fermentum pharetra.`,
-    `Aliquam id orci ut lectus varius viverra.`,
-    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
-    `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
-    `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
-    `Sed sed nisi sed augue convallis suscipit in sed felis.`,
-    `Aliquam erat volutpat.`,
-    `Nunc fermentum tortor ac porta dapibus.`,
-    `In rutrum ac purus sit amet tempus.`
-  ],
+export const  descriptions = [
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+  `Fusce tristique felis at fermentum pharetra.`,
+  `Aliquam id orci ut lectus varius viverra.`,
+  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+  `Aliquam erat volutpat.`,
+  `Nunc fermentum tortor ac porta dapibus.`,
+  `In rutrum ac purus sit amet tempus.`
+];
+
+export const options = [
+  { name: `Add luggage`, cost: 10 },
+  { name: `Switch to comfort class`, cost: 150 },
+  { name: `Add meal`, cost: 2 },
+  { name: `Choose seats`, cost: 9 },
+];
+
+export const activity = {
+  transfer: [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`],
+  activity: [`Check-in`, `Sightseeing`, `Restaurant`]
+};
+export const citys = [`Moscow`, `Sain-Petersburg`, `Porto`, `Lisboa`];
+
+export const getPoint = (price, {type, number}, cityNumber, descriptionNumber, optionsNumber ) => ({
+  action:  {
+    type,
+    name: activity[type][number]
+  },
+  city: citys[cityNumber],
+  images: new Array(Math.floor(Math.random() * 7)).fill(``).map((item) => `http://picsum.photos/300/150?r=${Math.random()}`),
+  description: new Array(descriptionNumber.length).fill(``).map((item, index) => descriptions[index]).join(` `),
   timeStart: Date.now() + Math.floor(Math.random() * 10),
   timeEnd: Date.now() + Math.floor(Math.random() * 10 * 2),
   price,
-  options: [
-    { name: `Add luggage`, cost: 10 },
-    { name: `Switch to comfort class`, cost: 150 },
-    { name: `Add meal`, cost: 2 },
-    { name: `Choose seats`, cost: 9 },
-  ]
+  selectedOptions: optionsNumber.map((number) => options[number]),
+  isFavorite: Boolean(Math.round(Math.random()))
 });
+
+
 
 export const getFilter = () => [`Everything`, `Future`, `Past`];
 
