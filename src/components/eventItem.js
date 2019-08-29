9a81eb4446a89aeacd9103685634ae53525a5473
Никитин Петр <nikitin@MacBook-Pro-Admin.local>
2019-08-29 22:47:36 +0300

    refactor event

--- src/components/eventItem.js
+++ src/components/eventItem.js
@@ -1,9 +1,9 @@
-export const eventItem = ({ icon, action, city, image, description, timeStart, timeEnd, price, options }, optionsNumbers) => `
+export const eventItem = ({ action , city, images, description, timeStart, timeEnd, price, selectedOptions, isFavorite } ) => `
     <div class="event">
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/${icon}.png" alt="${icon}">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${action.name.toLowerCase()}.png" alt="${action.name.toLowerCase()}">
       </div>
-      <h3 class="event__title">${action} ${city}</h3>
+      <h3 class="event__title">${action.name} ${ action.type === `transfer` ? `to ` : `at `} ${city}</h3>
 
       <div class="event__schedule">
         <p class="event__time">
@@ -21,11 +21,11 @@ export const eventItem = ({ icon, action, city, image, description, timeStart, t
       <h4 class="visually-hidden">Offers:</h4>
       <ul class="event__selected-offers">
     
-        ${optionsNumbers.map((optionNumber) => `
+        ${selectedOptions.map((option) => `
           <li class="event__offer">
-          <span class="event__offer-title">${options[optionNumber ].name}</span>
+          <span class="event__offer-title">${option.name}</span>
           &plus;
-          &euro;&nbsp;<span class="event__offer-price">${options[optionNumber].cost}</span>
+          &euro;&nbsp;<span class="event__offer-price">${option.cost}</span>
           </li>
         `).join(``)}
       </ul>
