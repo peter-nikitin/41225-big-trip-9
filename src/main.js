5fc2ed62f575c50d45c944e332b990f78159dae4
Никитин Петр <nikitin@MacBook-Pro-Admin.local>
2019-09-02 21:54:28 +0300

    refactor points rendering

--- src/main.js
+++ src/main.js
@@ -2,44 +2,83 @@ import {tripInfo as tripInfoLayout} from './components/tripInfo.js';
 import {menu as menuLayout} from './components/menu.js';
 import {filtersLayout} from './components/filters.js';
 
+import Event from './components/eventItem';
+import EventEdit from './components/eventEdit';
 
 import {sort as sortLayout} from './components/sort.js';
-import {getPoint, getFilter, getDays, getTripInfo} from './data.js';
+import {getPoint, getFilter, getTripInfo, getDays} from './data.js';
 import {renderTripDay} from './renderTripDay.js';
-import {render} from './render.js';
+import {render, Position, unRender} from './utils';
 
+const mockPoints = new Array(3).fill(``).map(() => getPoint(Math.floor(Math.random() * 100), {type: `activity`, number: 2}));
 
-const days = getDays(1, new Array(3).fill(``).map(() => getPoint(
-    Math.floor(Math.random() * 100),
-    {type: `activity`, number: 2})));
-
-const getTotal = (daysCount) => {
+const getTotal = (points) => {
   let total = 0;
-  daysCount.map((day) => day.points.map((point) => {
+  points.map((point) => {
     total += point.price;
     point.selectedOptions.map((option) => {
       if (option) {
-        console.log(option);
         total += option.cost;
       }
     });
-  }));
+  });
   return total;
 };
 
 const tripInfoContainer = document.querySelector(`.trip-main__trip-info `);
 const tripTotal = document.querySelector(`.trip-info__cost-value `);
-tripTotal.textContent = getTotal(days);
+tripTotal.textContent = getTotal(mockPoints);
+
 const menu = document.querySelector(`.trip-main__trip-controls`);
 const tripEvents = document.querySelector(`.trip-events`);
 
-render(tripInfoContainer, tripInfoLayout(getTripInfo(days)), `afterbegin`);
-render(menu, menuLayout(), `afterbegin`);
-render(menu, filtersLayout(getFilter()), `beforeend`);
-render(tripEvents, sortLayout(), `afterbegin`);
+tripInfoContainer.innerHTML = tripInfoLayout(getTripInfo(mockPoints));
+menu.insertAdjacentHTML(`afterbegin`, menuLayout());
+menu.insertAdjacentHTML(`beforeend`, filtersLayout(getFilter()));
+tripEvents.insertAdjacentHTML(`afterbegin`, sortLayout());
 
 const tripDays = document.createElement(`ul`);
 tripDays.classList.add(`trip-days`);
 
-days.map((day, index) => tripDays.appendChild(renderTripDay(index + 1, day.points)));
+[...getDays(mockPoints)].map((day, index) => tripDays.appendChild(renderTripDay(day, index)));
 tripEvents.appendChild(tripDays);
+
+const eventsContainer = document.querySelector(`.trip-events__list`);
+
+const renderPoint = (mockPoint) => {
+  const event = new Event(mockPoint);
+  const eventEdit = new EventEdit(mockPoint);
+
+  event.getElement()
+    .querySelector(`.event__rollup-btn`)
+    .addEventListener(`click`, () => {
+      eventsContainer.replaceChild(eventEdit.getElement(), event.getElement());
+    });
+
+  eventEdit.getElement()
+    .querySelector(`.event__rollup-btn`)
+    .addEventListener(`click`, () => {
+      eventsContainer.replaceChild(event.getElement(), eventEdit.getElement());
+    });
+  eventEdit.getElement()
+    .querySelector(`.event__save-btn`)
+    .addEventListener(`click`, () => {
+      eventsContainer.replaceChild(event.getElement(), eventEdit.getElement());
+    });
+
+  eventEdit.getElement()
+    .querySelector(`form`)
+    .addEventListener(`submit`, () => {
+      eventsContainer.replaceChild(event.getElement(), eventEdit.getElement());
+    });
+
+  eventEdit.getElement()
+    .querySelector(`.event__reset-btn`)
+    .addEventListener(`click`, () => {
+      unRender(eventEdit.getElement());
+      eventEdit.removeElement();
+    });
+
+  render(eventsContainer, event.getElement(), Position.BEFOREEND);
+};
+mockPoints.map((point) => renderPoint(point));
