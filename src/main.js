b2be7215800e46714cf31fd773e0898cc7ad9a39
Petr Nikitin <nikitin.p.94@gmail.com>
2019-09-11 00:52:44 +0300

    small cahnges

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {getPoint, getFilter, getTripInfo} from './data.js';
 
 import TripController from './controllers/tripController';
 
-const mockPoints = new Array(3).fill(``).map(() => getPoint(Math.floor(Math.random() * 100), {type: `activity`, number: 2}));
+const mockPoints = new Array(3).fill(``).map(() => getPoint(Math.floor(Math.random() * 100), Math.floor(Math.random() * 11)));
 
 const getTotal = (points) => {
   let total = 0;
